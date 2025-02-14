a5b2f8613014b96235215088e4c2e221541bd648
Paul Elliott <paul.elliott@kitware.com>
2025-02-14 16:36:38 -0500

    feat: remove vtk window and setup hot reload
    Cleans some boilerplate and sets up trame's hot
    reload dev hack. Run align-app --hot-reload and
    a reload button appears for quick GUI tweaks.

--- align_app/app/core.py
+++ align_app/app/core.py
@@ -1,24 +1,17 @@
 from trame.app import get_server
-from trame.decorators import TrameApp, change, controller
-from trame.ui.vuetify3 import SinglePageLayout
-from trame.widgets import vuetify3, vtk
+from trame.decorators import TrameApp, controller
+from . import ui
 
-
-# ---------------------------------------------------------
-# Engine class
-# ---------------------------------------------------------
 
 @TrameApp()
-class MyTrameApp:
+class AlignApp:
     def __init__(self, server=None):
         self.server = get_server(server, client_type="vue3")
         if self.server.hot_reload:
             self.server.controller.on_server_reload.add(self._build_ui)
-        self.ui = self._build_ui()
+        self._build_ui()
 
-        # Set state variable
-        self.state.trame__title = "align-app"
-        self.state.resolution = 6
+        self.reset_state()
 
     @property
     def state(self):
@@ -28,42 +21,14 @@ class MyTrameApp:
     def ctrl(self):
         return self.server.controller
 
-    @controller.set("reset_resolution")
-    def reset_resolution(self):
-        self.state.resolution = 6
-
-    @change("resolution")
-    def on_resolution_change(self, resolution, **kwargs):
-        print(f">>> ENGINE(a): Slider updating resolution to {resolution}")
+    @controller.set("reset_state")
+    def reset_state(self):
+        self.state.prompt = "This is a test prompt and only a test prompt."
 
     def _build_ui(self, *args, **kwargs):
-        with SinglePageLayout(self.server) as layout:
-            # Toolbar
-            layout.title.set_text("Trame / vtk.js")
-            with layout.toolbar:
-                vuetify3.VSpacer()
-                vuetify3.VSlider(                    # Add slider
-                    v_model=("resolution", 6),      # bind variable with an initial value of 6
-                    min=3, max=60, step=1,          # slider range
-                    dense=True, hide_details=True,  # presentation setup
-                )
-                with vuetify3.VBtn(icon=True, click=self.ctrl.reset_camera):
-                    vuetify3.VIcon("mdi-crop-free")
-                with vuetify3.VBtn(icon=True, click=self.reset_resolution):
-                    vuetify3.VIcon("mdi-undo")
+        extra_args = {}
+        if self.server.hot_reload:
+            ui.reload(ui)
+            extra_args["reload"] = self._build_ui
 
-            # Main content
-            with layout.content:
-                with vuetify3.VContainer(fluid=True, classes="pa-0 fill-height"):
-                    with vtk.VtkView() as vtk_view:                # vtk.js view for local rendering
-                        self.ctrl.reset_camera = vtk_view.reset_camera  # Bind method to controller
-                        with vtk.VtkGeometryRepresentation():      # Add representation to vtk.js view
-                            vtk.VtkAlgorithm(                      # Add ConeSource to representation
-                                vtk_class="vtkConeSource",          # Set attribute value with no JS eval
-                                state=("{ resolution }",)          # Set attribute value with JS eval
-                            )
-
-            # Footer
-            # layout.footer.hide()
-
-            return layout
+        self.ui = ui.AlignLayout(self.server, **extra_args)
--- align_app/app/main.py
+++ align_app/app/main.py
@@ -1,8 +1,10 @@
-from .core import MyTrameApp
+from .core import AlignApp
+
 
 def main(server=None, **kwargs):
-    app = MyTrameApp(server)
+    app = AlignApp(server)
     app.server.start(**kwargs)
+
 
 if __name__ == "__main__":
     main()
