cfd93716de88fd60e97ae6462fc2bce4320f2e4b
Paul Elliott <paul.elliott@kitware.com>
2025-02-17 16:13:55 -0500

    feat: init align library

--- align_app/app/core.py
+++ align_app/app/core.py
@@ -1,6 +1,7 @@
 from trame.app import get_server
 from trame.decorators import TrameApp, controller
 from . import ui
+from ..adm.decider import get_decision
 
 
 @TrameApp()
@@ -23,21 +24,21 @@ class AlignApp:
 
     @controller.set("reset_state")
     def reset_state(self):
-        self.state.prompt = "This is a test prompt and only a test prompt."
-        self.state.output = ["This is a test output and only a test output."]
+        self.state.prompt = ""
+        self.state.output = []
 
     @controller.set("submit_prompt")
     def submit_prompt(self):
+        decision = get_decision(self.state.prompt)
         self.state.output = self.state.output = [
             *self.state.output,
-            f"Processed: {self.state.prompt}",
+            decision,
         ]
-        self.state.prompt = "moar text"
+        self.state.prompt = ""
 
     def _build_ui(self, *args, **kwargs):
         extra_args = {}
         if self.server.hot_reload:
             ui.reload(ui)
             extra_args["reload"] = self._build_ui
-
         self.ui = ui.AlignLayout(self.server, **extra_args)
