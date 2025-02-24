022d1f198b42ba31ba3691a974504e91df2a78fd
Paul Elliott <paul.elliott@kitware.com>
2025-02-24 14:49:06 -0500

    feat: add waiting for decision spinner

--- align_app/app/core.py
+++ align_app/app/core.py
@@ -1,4 +1,4 @@
-from trame.app import get_server
+from trame.app import get_server, asynchronous
 from trame.decorators import TrameApp, controller
 from . import ui
 from ..adm.decider import get_prompt, get_decision, readable_scenario, serialize_prompt
@@ -31,15 +31,27 @@ class AlignApp:
         self.update_prompt()
         self.state.output = []
 
+    async def make_decision(self):
+        prompt = {"prompt": serialize_prompt(self._prompt)}
+        with self.state:
+            self.state.output = [
+                *self.state.output,
+                prompt,
+            ]
+        await self.server.network_completion  # let spinner be shown
+
+        decision = get_decision(self._prompt)
+        result = {"prompt": prompt["prompt"], "decision": decision}
+        with self.state:
+            self.state.output = [
+                *self.state.output[:-1],
+                result,
+            ]
+            self.update_prompt()
+
     @controller.set("submit_prompt")
     def submit_prompt(self):
-        decision = get_decision(self._prompt)
-        result = {"prompt": serialize_prompt(self._prompt), "decision": decision}
-        self.state.output = [
-            *self.state.output,
-            result,
-        ]
-        self.update_prompt()
+        asynchronous.create_task(self.make_decision())
 
     def _build_ui(self, *args, **kwargs):
         extra_args = {}
