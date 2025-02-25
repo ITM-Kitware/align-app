012da71663ecbffd104848646d1ea1874180c1b3
Paul Elliott <paul.elliott@kitware.com>
2025-02-25 16:19:40 -0500

    feat: add select input for scenario

--- align_app/app/core.py
+++ align_app/app/core.py
@@ -1,17 +1,18 @@
 from trame.app import get_server, asynchronous
 from trame.decorators import TrameApp, controller
 from . import ui
-from ..adm.decider import get_prompt, get_decision, readable_scenario, serialize_prompt
+from ..adm.decider import get_decision, serialize_prompt
+from .prompt import PromptController
 
 
 @TrameApp()
 class AlignApp:
     def __init__(self, server=None):
         self.server = get_server(server, client_type="vue3")
+        self._promptController = PromptController(self.server)
         if self.server.hot_reload:
             self.server.controller.on_server_reload.add(self._build_ui)
         self._build_ui()
-
         self.reset_state()
 
     @property
@@ -22,32 +23,28 @@ class AlignApp:
     def ctrl(self):
         return self.server.controller
 
-    def update_prompt(self):
-        self._prompt = get_prompt()
-        self.state.prompt = readable_scenario(self._prompt["scenario"])
-
     @controller.set("reset_state")
     def reset_state(self):
-        self.update_prompt()
+        self._promptController.reset()
         self.state.output = []
 
     async def make_decision(self):
-        prompt = {"prompt": serialize_prompt(self._prompt)}
+        prompt = self._promptController.get_prompt()
+        run = {"prompt": serialize_prompt(prompt)}
         with self.state:
             self.state.output = [
                 *self.state.output,
-                prompt,
+                run,
             ]
         await self.server.network_completion  # let spinner be shown
 
-        decision = get_decision(self._prompt)
-        result = {"prompt": prompt["prompt"], "decision": decision}
+        decision = get_decision(prompt)
+        run = {"prompt": run["prompt"], "decision": decision}
         with self.state:
             self.state.output = [
                 *self.state.output[:-1],
-                result,
+                run,
             ]
-            self.update_prompt()
 
     @controller.set("submit_prompt")
     def submit_prompt(self):
