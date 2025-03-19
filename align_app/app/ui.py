11860b00b607692808fc8505b66b7bed80eae956
Paul Elliott <paul.elliott@kitware.com>
2025-03-19 10:37:52 -0400

    refactor(ui): break rows into classes

--- align_app/app/ui.py
+++ align_app/app/ui.py
@@ -1,5 +1,8 @@
 from trame.ui.vuetify3 import SinglePageLayout
 from trame.widgets import vuetify3, html
+from ..utils.utils import noop
+
+MAX_ALIGNMENT_ATTRIBUTES = 1
 
 
 def reload(m=None):
@@ -12,11 +15,155 @@ class UnorderedObject(html.Ul):
         super().__init__(**kwargs)
         with self:
             with html.Li(v_for=(f"[key, value] in Object.entries({obj})",)):
-                html.Span("{{key}}: ", style="font-weight: bold")
+                html.Span("{{key}}: ")
                 html.Span("{{value}}")
 
 
-class Scenario(vuetify3.VExpansionPanel):
+class PanelSection(vuetify3.VExpansionPanel):
+    def __init__(self, child, **kwargs):
+        super().__init__(**kwargs)
+
+        has_text = hasattr(child, "Text")
+        with self:
+            # set icons to blank to keep column alignment.  hide-actions changes width of available space.
+            icon_kwargs = (
+                {"expand_icon": "", "collapse_icon": ""} if not has_text else {}
+            )
+            with vuetify3.VExpansionPanelTitle(
+                static=not has_text, **icon_kwargs, classes="text-subtitle-1"
+            ):
+                child.Title()
+            if has_text:
+                with vuetify3.VExpansionPanelText():
+                    child.Text()
+
+
+class RowWithLabel:
+    def __init__(self, run_content=noop, label="", title=True):
+        with vuetify3.VRow():
+            with vuetify3.VCol(cols=2):
+                html.Span(label, classes="font-weight-bold")
+            with vuetify3.VCol(
+                v_for=("id in runs_to_compare",),
+                key=("id",),
+                classes="text-no-wrap text-truncate" if title else "",
+            ):
+                run_content()
+
+
+class LlmBackbone:
+    class Title:
+        def __init__(self):
+            def run_content():
+                html.Span("{{runs[id].prompt.decider_params.llm_backbone}}")
+
+            RowWithLabel(run_content=run_content, label="LLM Backbone")
+
+
+class DecisionMaker:
+    class Title:
+        def __init__(self):
+            def run_content():
+                html.Span("{{runs[id].prompt.decider_params.decider}}")
+
+            RowWithLabel(run_content=run_content, label="Decision Maker", title=False)
+
+
+class AlignmentTargets:
+    class Title:
+        def __init__(self):
+            def run_content():
+                html.Span(
+                    "{{ runs[id].prompt.alignment_targets.length ? "
+                    "runs[id].prompt.alignment_targets.map(att => att.id).join(', ') : "
+                    "'No Alignment Targets' }}"
+                )
+
+            RowWithLabel(run_content=run_content, label="Alignment Target")
+
+    class Text:
+        def __init__(self):
+            def run_content():
+                with html.Div(
+                    v_for=("attribute in runs[id].prompt.alignment_targets",),
+                    key=("attribute.id",),
+                    classes="mb-4",
+                ):
+                    with html.Ul(classes="ml-8"):
+                        with html.Li(v_for=("value in attribute.kdma_values",)):
+                            html.Span("{{value.kdma}}: ")
+                            html.Span("{{value.value}}")
+                html.Div("", v_if=("runs[id].prompt.alignment_targets.length === 0",))
+
+            RowWithLabel(run_content=run_content, title=False)
+
+
+class Scenario:
+    class Title:
+        def __init__(self):
+            def run_content():
+                html.Span(
+                    "{{runs[id].prompt.scenario.scenario_id}} - "
+                    "{{runs[id].prompt.scenario.full_state.unstructured}}",
+                )
+
+            RowWithLabel(run_content=run_content, label="Scenario")
+
+    class Text:
+        def __init__(self):
+            def run_content():
+                html.P(
+                    "{{runs[id].prompt.scenario.full_state.unstructured}}",
+                    classes="text-subtitle-1 pb-4",
+                )
+                html.H4("Choices")
+                with html.Ul(
+                    v_for=("choice in runs[id].prompt.scenario.choices",),
+                    classes="ml-8",
+                ):
+                    html.Li("{{choice.unstructured}}")
+
+            RowWithLabel(run_content=run_content, title=False)
+
+
+class Decision:
+    class Title:
+        def __init__(self):
+            def render_run_decision():
+                html.Span(
+                    "{{runs[id].decision.unstructured}}", v_if=("runs[id].decision",)
+                )
+                vuetify3.VProgressCircular(v_else=True, indeterminate=True, size=20)
+
+            RowWithLabel(run_content=render_run_decision, label="Decision")
+
+    class Text:
+        def __init__(self):
+            def render_run_decision_text():
+                with html.Template(v_if=("runs[id].decision",)):
+                    html.H4("Justification")
+                    html.P("{{runs[id].decision.justification}}")
+                    html.H4("KDMA Association", classes="mt-4")
+                    UnorderedObject(
+                        "runs[id].decision.kdma_association", classes="ml-8"
+                    )
+
+            RowWithLabel(render_run_decision_text, title=False)
+
+
+class ResultsComparison(html.Div):
+    def __init__(self, **kwargs):
+        super().__init__(classes="d-flex flex-wrap ga-4 pa-1", **kwargs)
+        with self:
+            with vuetify3.VExpansionPanels(multiple=True, variant="accordion"):
+                PanelSection(child=LlmBackbone)
+                PanelSection(child=DecisionMaker)
+                PanelSection(child=AlignmentTargets)
+                PanelSection(child=Scenario)
+                PanelSection(child=Decision)
+
+
+class ScenarioPanel(vuetify3.VExpansionPanel):
     def __init__(self, scenario, **kwargs):
         super().__init__(**kwargs)
         with self:
@@ -38,237 +185,6 @@ class Scenario(vuetify3.VExpansionPanel):
                     classes="ml-8",
                 ):
                     html.Li("{{choice.unstructured}}")
-
-
-class DeciderParams(vuetify3.VExpansionPanel):
-    def __init__(self, params, **kwargs):
-        super().__init__(**kwargs)
-        with self:
-            with vuetify3.VExpansionPanelTitle():
-                with vuetify3.VRow(classes="text-h6 text-no-wrap text-truncate"):
-                    with vuetify3.VCol():
-                        html.Span("LLM Backbone: ", classes="font-weight-bold")
-                        html.Span(f"{{{{{params}.llm_backbone}}}}")
-                    with vuetify3.VCol():
-                        html.Span("Decision Maker: ", classes="font-weight-bold")
-                        html.Span(f"{{{{{params}.decider}}}}")
-            with vuetify3.VExpansionPanelText():
-                UnorderedObject(params)
-
-
-class AlignmentTargets(vuetify3.VExpansionPanel):
-    def __init__(self, alignment_targets, **kwargs):
-        super().__init__(**kwargs)
-        with self:
-            with vuetify3.VExpansionPanelTitle():
-                with html.Div(classes="text-h6 text-no-wrap text-truncate"):
-                    html.Span("Alignment Targets: ", classes="font-weight-bold")
-                    html.Span(
-                        f"{{{{ {alignment_targets}.length ? "
-                        f"{alignment_targets}.map(att => att.id).join(', ') : "
-                        f"'No Alignment' }}}}"
-                    )
-            with vuetify3.VExpansionPanelText():
-                with html.Div(
-                    v_for=(f"attribute in {alignment_targets}",),
-                    classes="mb-4",
-                ):
-                    html.H3("{{attribute.id}}")
-                    UnorderedObject("attribute", classes="ml-8")
-                html.Div(
-                    "No Alignment",
-                    v_if=(f"{alignment_targets}.length === 0",),
-                )
-
-
-class Prompt:
-    def __init__(self, prompt):
-        DeciderParams(f"{prompt}.decider_params")
-        AlignmentTargets(f"{prompt}.alignment_targets")
-        Scenario(f"{prompt}.scenario")
-
-
-class Decision:
-    def __init__(self, decision):
-        with vuetify3.VExpansionPanel():
-            with vuetify3.VExpansionPanelTitle():
-                with html.Div(classes="text-h6 text-no-wrap text-truncate"):
-                    html.Span("Decision: ", classes="font-weight-bold")
-                    html.Span(
-                        f"{{{{{decision}.unstructured}}}}",
-                        v_if=(f"{decision}",),
-                    )
-                    vuetify3.VProgressCircular(v_else=True, indeterminate=True)
-            with vuetify3.VExpansionPanelText(v_if=(f"{decision}",)):
-                html.H3("Justification")
-                html.P(f"{{{{{decision}.justification}}}}")
-                html.H3("KDMA Association")
-                UnorderedObject(f"{decision}.kdma_association", classes="ml-8")
-
-
-class Result(vuetify3.VCard):
-    def __init__(self, result, **kwargs):
-        super().__init__(**kwargs)
-        with self:
-            with vuetify3.VCardText():
-                with vuetify3.VExpansionPanels(multiple=True, variant="accordion"):
-                    Prompt(f"{result}.prompt")
-                    Decision(f"{result}.decision")
-
-
-class ResultsComparison(html.Div):
-    def __init__(self, **kwargs):
-        super().__init__(classes="d-flex flex-wrap ga-4 pa-1", **kwargs)
-        with self:
-            with vuetify3.VExpansionPanels(multiple=True, variant="accordion"):
-                # llm backbone
-                with vuetify3.VExpansionPanel():
-                    with vuetify3.VExpansionPanelTitle():
-                        with vuetify3.VRow():
-                            with vuetify3.VCol():
-                                html.Span("LLM Backbone: ", classes="font-weight-bold")
-                            with vuetify3.VCol(
-                                v_for=("id in runs_to_compare",),
-                                key=("id",),
-                            ):
-                                html.Span(
-                                    "{{runs[id].prompt.decider_params.llm_backbone}}",
-                                )
-                # decision maker
-                with vuetify3.VExpansionPanel():
-                    with vuetify3.VExpansionPanelTitle():
-                        with vuetify3.VRow():
-                            with vuetify3.VCol():
-                                html.Span(
-                                    "Decision Maker: ", classes="font-weight-bold"
-                                )
-                            with vuetify3.VCol(
-                                v_for=("id in runs_to_compare",),
-                                key=("id",),
-                            ):
-                                html.Span(
-                                    "{{runs[id].prompt.decider_params.decider}}",
-                                )
-                # alignment targets
-                with vuetify3.VExpansionPanel():
-                    with vuetify3.VExpansionPanelTitle():
-                        with vuetify3.VRow():
-                            with vuetify3.VCol():
-                                html.Span(
-                                    "Alignment Targets: ", classes="font-weight-bold"
-                                )
-                            with vuetify3.VCol(
-                                v_for=("id in runs_to_compare",),
-                                key=("id",),
-                            ):
-                                html.Span(
-                                    "{{ runs[id].prompt.alignment_targets.length ? "
-                                    "runs[id].prompt.alignment_targets.map(att => att.id).join(', ') : "
-                                    "'No Alignment Targets' }}"
-                                )
-                    with vuetify3.VExpansionPanelText():
-                        with vuetify3.VRow():
-                            with vuetify3.VCol():
-                                html.Span()
-                            with vuetify3.VCol(
-                                v_for=("id in runs_to_compare",),
-                                key=("id",),
-                            ):
-                                with html.Div(
-                                    v_for=(
-                                        "attribute in runs[id].prompt.alignment_targets",
-                                    ),
-                                    key=("attribute.id",),
-                                    classes="mb-4",
-                                ):
-                                    html.P("{{attribute.id}}")
-                                    with html.Ul(classes="ml-8"):
-                                        with html.Li(
-                                            v_for=("value in attribute.kdma_values",)
-                                        ):
-                                            html.Span(
-                                                "{{value.kdma}}: ",
-                                            )
-                                            html.Span("{{value.value}}")
-                                html.Div(
-                                    "",
-                                    v_if=(
-                                        "runs[id].prompt.alignment_targets.length === 0",
-                                    ),
-                                )
-                # scenarios
-                with vuetify3.VExpansionPanel():
-                    with vuetify3.VExpansionPanelTitle():
-                        with vuetify3.VRow():
-                            with vuetify3.VCol():
-                                html.Span("Scenarios: ", classes="font-weight-bold")
-                            with vuetify3.VCol(
-                                v_for=("id in runs_to_compare",),
-                                key=("id",),
-                                classes="text-no-wrap text-truncate",
-                            ):
-                                html.Span(
-                                    "{{runs[id].prompt.scenario.scenario_id}} - "
-                                    "{{runs[id].prompt.scenario.full_state.unstructured}}",
-                                )
-                    with vuetify3.VExpansionPanelText():
-                        with vuetify3.VRow():
-                            vuetify3.VCol()
-                            with vuetify3.VCol(
-                                v_for=("id in runs_to_compare",),
-                                key=("id",),
-                            ):
-                                html.P(
-                                    "{{runs[id].prompt.scenario.full_state.unstructured}}",
-                                    classes="text-subtitle-1 pb-4",
-                                )
-                                html.P("Choices")
-                                with html.Ul(
-                                    v_for=(
-                                        "choice in runs[id].prompt.scenario.choices"
-                                    ),
-                                    classes="ml-8",
-                                ):
-                                    html.Li("{{choice.unstructured}}")
-
-                # Decision
-                with vuetify3.VExpansionPanel():
-                    with vuetify3.VExpansionPanelTitle():
-                        with vuetify3.VRow():
-                            with vuetify3.VCol():
-                                html.Span(
-                                    "Decision: ",
-                                    classes="font-weight-bold",
-                                )
-                            with vuetify3.VCol(
-                                v_for=("id in runs_to_compare",),
-                                key=("id",),
-                                classes="text-no-wrap text-truncate",
-                            ):
-                                html.Span(
-                                    "{{runs[id].decision.unstructured}}",
-                                    v_if=("runs[id].decision",),
-                                )
-                                vuetify3.VProgressCircular(
-                                    v_else=True, indeterminate=True, size=20
-                                )
-                    with vuetify3.VExpansionPanelText():
-                        with vuetify3.VRow():
-                            vuetify3.VCol()
-                            with vuetify3.VCol(
-                                v_for=("id in runs_to_compare",),
-                                key=("id",),
-                            ):
-                                with html.Template(
-                                    v_if=("runs[id].decision",),
-                                ):
-                                    html.H3("Justification")
-                                    html.P("{{runs[id].decision.justification}}")
-                                    html.H3("KDMA Association")
-                                    UnorderedObject(
-                                        "runs[id].decision.kdma_association",
-                                        classes="ml-8",
-                                    )
 
 
 class PromptInput(vuetify3.VCard):
@@ -326,7 +242,9 @@ class PromptInput(vuetify3.VCard):
                             ):
                                 vuetify3.VIcon("mdi-delete")
 
-                with vuetify3.VRow(v_if=("possible_alignment_attributes.length > 0",)):
+                with vuetify3.VRow(
+                    v_if=(f"alignment_attributes.length < {MAX_ALIGNMENT_ATTRIBUTES}",)
+                ):
                     with vuetify3.VCol():
                         vuetify3.VBtn(
                             "Add Alignment Attribute",
@@ -346,7 +264,7 @@ class PromptInput(vuetify3.VCard):
                         with vuetify3.VExpansionPanels(
                             multiple=True, variant="accordion"
                         ):
-                            Scenario("prompt_scenario")
+                            ScenarioPanel("prompt_scenario")
 
             with vuetify3.VCardActions():
                 with vuetify3.VBtn(click=self.server.controller.submit_prompt):
